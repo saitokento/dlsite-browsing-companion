aabc66b4cdea43b0c3b8b98927275f6dd3a26853
saitokento <246199479+saitokento@users.noreply.github.com>
2025-12-01 17:27:42 +0900

    build: #1 最小限のマニフェストを設定

--- entrypoints/content.ts
+++ entrypoints/content.ts
@@ -1,6 +1,6 @@
 export default defineContentScript({
-  matches: ["*://*.google.com/*"],
+  matches: ["*://*.dlsite.com/*"],
   main() {
-    console.log("Hello content.");
+    //console.log("Hello content.");
   },
 });
--- wxt.config.ts
+++ wxt.config.ts
@@ -3,4 +3,9 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
+  manifest: {
+    name: "DLsite Browsing Companion",
+    description: "",
+    version: "0.0.0",
+  },
 });
