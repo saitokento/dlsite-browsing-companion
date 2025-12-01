92d72ec2efdebdac4c370e899f30a21496134802
saitokento <246199479+saitokento@users.noreply.github.com>
2025-12-01 13:15:52 +0900

    chore: #1 eslint.config.jsにglobalIgnores、jsx-runtime、Reactのバージョンを追加

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,12 @@ import js from "@eslint/js";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-import { defineConfig } from "eslint/config";
+import { defineConfig, globalIgnores } from "eslint/config";
 import autoImports from "./.wxt/eslint-auto-imports.mjs";
 import reactHooks from "eslint-plugin-react-hooks";
 
 export default defineConfig([
+  globalIgnores([".output/**", ".wxt/**"]),
   autoImports,
   {
     files: ["**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
@@ -16,5 +17,13 @@ export default defineConfig([
   },
   tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  pluginReact.configs.flat["jsx-runtime"],
+  {
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
   reactHooks.configs.flat.recommended,
 ]);
