50f41f177a84ae9b8419862eb1287c5cddba232e
saitokento <246199479+saitokento@users.noreply.github.com>
2025-12-06 02:22:08 +0900

    fix: #5 クーポン利用価格はnullableに変更

--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,13 +1,13 @@
 export interface Price {
   prefix: string;
-  amount: number;
+  amount: number | null;
   suffix: string;
 }
 
 export interface WorkInfo {
   name: string;
   price: Price;
-  coupon_price: Price;
+  coupon_price: Price | null;
   genres: string[];
   description: string;
 }
