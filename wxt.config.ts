60530f3d79528a2e481e880a23740908ac3aecc6
saitokento <246199479+saitokento@users.noreply.github.com>
2026-04-20 10:10:15 +0900

    feat: #84 startUrlsをChromeの新しいタブに変更

--- wxt.config.ts
+++ wxt.config.ts
@@ -3,6 +3,9 @@ import { defineConfig } from "wxt";
 // See https://wxt.dev/api/config.html
 export default defineConfig({
   modules: ["@wxt-dev/module-react"],
+  webExt: {
+    startUrls: ["chrome://newtab/"],
+  },
   manifest: {
     name: "DLsite Browsing Companion",
     description: "",
